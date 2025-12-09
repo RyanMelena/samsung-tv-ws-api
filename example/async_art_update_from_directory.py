ee1a7462a70617399997d0e5d9b012282dadfe30
RyanMelena <Ryan.Melena@gmail.com>
2025-12-09 01:45:30 -0600

    Update async_art_update_from_directory.py

--- example/async_art_update_from_directory.py
+++ example/async_art_update_from_directory.py
@@ -77,7 +77,7 @@ def parseargs():
     parser = argparse.ArgumentParser(description='Async Upload images to Samsung TV Version: {}'.format(__version__))
     parser.add_argument('ip', action="store", type=str, default=None, help='ip address of TV (default: %(default)s))')
     parser.add_argument('-f','--folder', action="store", type=str, default="./images", help='folder to load images from (default: %(default)s))')
-    parser.add_argument('-d','--data_dir', action='store_true', default="./", help='default data directory (default: %(default)s))')
+    parser.add_argument('-d','--data_dir', action='store', default="./", help='default data directory (default: %(default)s))')
     parser.add_argument('-m','--matte', action="store", type=str, default="none", help='default matte to use (default: %(default)s))')
     parser.add_argument('-t','--token_file', action="store", type=str, default="token_file.txt", help='token file to use in data_dir (default: %(default)s))')
     parser.add_argument('-u','--update', action="store", type=float, default=0, help='slideshow update period (mins) 0=off (default: %(default)s))')
